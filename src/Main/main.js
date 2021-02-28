a6bbae2b5f5586cb24f09443ba7e340202f67809
Adam <benbourhim7@gmail.com>
2021-02-28 21:17:58 +0200

    comit

--- src/Main/main.js
+++ src/Main/main.js
@@ -1,24 +1,21 @@
-import Timer from "easytimer.js";
 import React, { useState, useEffect } from "react";
 import styles from "./main.module.css";
 function Main() {
   const [rung, setRung] = useState(false);
   const [intervalId, setIntervalId] = useState(null);
-  const mytimer = new Timer();
-  const minute = mytimer.getTotalTimeValues().minutes;
-  const second = mytimer.getTotalTimeValues().seconds;
-  const [timerM, setTimerM] = useState(minute + 25);
-  const [timerS, setTimerS] = useState(second);
+  const [timer, setTimer] = useState(5);
+
   useEffect(() => {
     if (rung) {
       const myVar = setInterval(() => {
-        setTimerM((timer) => timer + 1);
+        setTimer((timer) => timer - 1);
       }, 1000);
       setIntervalId(myVar);
     } else {
       clearInterval(intervalId);
     }
   }, [rung]);
+
   return (
     <div className={styles.container}>
       <div className={styles.box}>
@@ -26,9 +23,8 @@ function Main() {
         <div className={styles.btn}>Short Break</div>
         <div className={styles.btn}>Long Break</div>
       </div>
-      <div className={styles.box2}>
-        {timerM}:{timerS}
-      </div>
+
+      <div className={styles.box2}>{timer}</div>
       <div className={styles.box3}>
         <button>
           {rung ? (
