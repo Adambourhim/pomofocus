d10068f9588299fdb290fe88699ab2b58ca321b9
Adam <benbourhim7@gmail.com>
2021-03-01 12:53:29 +0200

    comit2

--- src/Main/main.js
+++ src/Main/main.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styles from "./main.module.css";
 function Main() {
-  const [rung, setRung] = useState(false);
+  const [rung, setRung] = useState(0);
   const [intervalId, setIntervalId] = useState(null);
-  const [timer, setTimer] = useState(5);
-
-  useEffect(() => {
+  const [timer, setTimer] = useState({ m: 0, s: 6 });
+  /* useEffect(() => {
     if (rung) {
       const myVar = setInterval(() => {
         setTimer((timer) => timer - 1);
@@ -14,8 +13,30 @@ function Main() {
     } else {
       clearInterval(intervalId);
     }
-  }, [rung]);
+  }, [rung]); */
 
+  var updateM = timer.m;
+  var updateS = timer.s;
+
+  const run = () => {
+    if (updateS === 0) {
+      updateM--;
+      updateS = 60;
+    }
+    updateS--;
+    return setTimer({ m: updateM, s: updateS });
+  };
+  const start = () => {
+    run();
+    setRung(1);
+
+    setIntervalId(setInterval(run, 1000));
+  };
+  const stop = () => {
+    clearInterval(intervalId);
+    setRung(0);
+  };
+  const stopWatch = () => {};
   return (
     <div className={styles.container}>
       <div className={styles.box}>
@@ -24,13 +45,16 @@ function Main() {
         <div className={styles.btn}>Long Break</div>
       </div>
 
-      <div className={styles.box2}>{timer}</div>
+      <div className={styles.box2}>
+        {timer.m >= 10 ? timer.m : "0" + timer.m} :
+        {timer.s >= 10 ? timer.s : "0" + timer.s}
+      </div>
       <div className={styles.box3}>
         <button>
-          {rung ? (
-            <span onClick={() => setRung(false)}>STOP</span>
+          {rung == 0 ? (
+            <span onClick={() => start()}>STAR</span>
           ) : (
-            <span onClick={() => setRung(true)}>STAR</span>
+            <span onClick={() => stop()}>STOP</span>
           )}
         </button>
       </div>
