4955e5cc2cdd0d17406ad1de8a92ddda9869769e
adambourhim <benbourhim7@gmail.com>
2021-08-10 18:12:37 +0300

    comit_header

--- src/Header/header.js
+++ src/Header/header.js
@@ -15,7 +15,7 @@ function Header() {
       <div className={styles.container}>
         <div>Promofocus</div>
         <div className={styles.box}>
-          <button className={styles.btn}>Report</button>
+          <button className={styles.btn}>Repor</button>
           <button className={styles.btn}>Setting</button>
           <button className={styles.btn}>Login</button>
         </div>
